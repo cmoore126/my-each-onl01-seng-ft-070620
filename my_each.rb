4a54a68468c5317de7119e38f477c1b8a1f0da1e
Clayton Moore <cmoorefour@gmail.com>
2020-07-17 21:45:00 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,7 +1,7 @@
 collection = [1, 2, 3, 4]
 def my_each() # put argument(s) here
   my_each(collection) do |words|
-    while i < collection.length
+    while i < words.length
       yield (collection[i])
       i = i + 1
     end
