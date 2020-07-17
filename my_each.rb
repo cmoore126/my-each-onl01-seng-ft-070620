8ab60ed9e567ff19352656e0ad29eea6aa1d845c
Clayton Moore <cmoorefour@gmail.com>
2020-07-17 20:47:16 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,16 @@
-def my_each # put argument(s) here
-  # code here
-end
+def my_each(array) # put argument(s) here
+  collection = [1, 2, 3, 4]
+  my_each(collection) do |i|
+    puts i
+  end
+
+
+ i = 0
+
+ while i < array.length
+  yield(array[i])
+  i = i + 1
+ end
+
+    array
+  end
