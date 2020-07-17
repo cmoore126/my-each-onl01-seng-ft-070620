8cedbe18f274d38a14b4512a9ec026da4f902f37
Clayton Moore <cmoorefour@gmail.com>
2020-07-17 20:52:33 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,12 +1,10 @@
 collection = [1, 2, 3, 4]
 def my_each() # put argument(s) here
   my_each(collection) do |i|
-    puts i
-  end
-end
-
-
     while i < array.length
       (array[i])
       i = i + 1
     end
+    puts i
+  end
+end
